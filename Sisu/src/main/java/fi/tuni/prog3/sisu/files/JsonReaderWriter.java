8eebd451cd488752e10f6ebcd88e251c29474b1e
txeejo <eero.jormalainen@tuni.fi>
2023-04-21 14:43:31 +0300

    added javadoc and error handling

--- Sisu/src/main/java/fi/tuni/prog3/sisu/files/JsonReaderWriter.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/files/JsonReaderWriter.java
@@ -3,65 +3,60 @@ package fi.tuni.prog3.sisu.files;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 import fi.tuni.prog3.sisu.Student;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.lang.ProcessBuilder.Redirect.Type;
-
+/**
+ * A class for writing and reading student -data from and to json -files.
+ */
 public class JsonReaderWriter{
     /**
      * Reads JSON from the given file.
-     * @param StudentNumber
-     * @return true if the read was successful, otherwise false.
-     * @throws Exception if the method e.g, cannot find the file.
+     * @param StudentNumber - The student number of the students whose info is wanted
+     * @return The data of the student with given student number
+     * @throws Exception if the method e.g, cannot find the file with the given student number.
      */
+    
     public Student readFromFile(String StudentNumber) throws Exception{
 
-        String path = "students\\";
-        JsonReader reader = new JsonReader(new FileReader(path + StudentNumber + ".json"));
-        JsonObject root = JsonParser.parseReader(reader).getAsJsonObject();
+        try {
+            String path = "students\\";
+            JsonReader reader = new JsonReader(new FileReader(path + StudentNumber + ".json"));
+            JsonObject root = JsonParser.parseReader(reader).getAsJsonObject();
         
-        String firstName = root.getAsJsonPrimitive("firstName").getAsString();
-        String lastName = root.getAsJsonPrimitive("lastName").getAsString();
 
-        String studentNumber = root.getAsJsonPrimitive("studentNumber").getAsString();
+            String firstName = root.getAsJsonPrimitive("firstName").getAsString();
+            String lastName = root.getAsJsonPrimitive("lastName").getAsString();
 
-        int startYear = root.getAsJsonPrimitive("startYear").getAsInt();
-        
-        int gradYear = root.getAsJsonPrimitive("gradYear").getAsInt();
-        
-        String degree = root.getAsJsonPrimitive("degree").getAsString();
-        
-        Student student = new Student(firstName, lastName, studentNumber, startYear, gradYear, degree);
-        JsonArray courses = root.getAsJsonArray("courses");
-        for(JsonElement course : courses){
-            student.addCompletedCourse(course.getAsString());
+            String studentNumber = root.getAsJsonPrimitive("studentNumber").getAsString();
+
+            int startYear = root.getAsJsonPrimitive("startYear").getAsInt();
+
+            int gradYear = root.getAsJsonPrimitive("gradYear").getAsInt();
+
+            Student student = new Student(firstName, lastName, studentNumber, startYear, gradYear);
+            JsonArray courses = root.getAsJsonArray("courses");
+            
+            for (JsonElement course : courses) {
+                student.addCompletedCourse(course.getAsString());
+            }
+            return student; 
         }
-        return student;
-        
+            catch (JsonIOException | JsonSyntaxException | FileNotFoundException e){
+                System.out.println("Couldn´t find a student with the given student number");
+                return null;
+        } 
     }
-    
-        public class StudentAdapter implements JsonSerializer<Student> {
-            @Override
-            public JsonElement serialize(Student student, java.lang.reflect.Type type, JsonSerializationContext jsc) {
 
-                    JsonObject object = new JsonObject();
-                    object.addProperty("firstName", student.getFirstName());
-                    object.addProperty("lastName", student.getLastName());
-                    object.addProperty("studentNumber", student.getStudentNumber());
-                    object.addProperty("startYear", student.getStartYear());
-                    object.addProperty("gradYear", student.getGradYear());
-
-                    return object;
-
-            }
-
-            public Student deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-                    return null;
-                }
-            }
-
-    
+        /**
+     * Writes JSON to the given file.
+     * @param student - The student whose info is wanted to write
+     * @return The data of the student with given student number
+     * @throws Exception if the method e.g, cannot find the file with the given student number.
+     */
     public boolean writeToFile(Student student) throws Exception {
+
         try (FileWriter writer = new FileWriter("students\\" + student.getStudentNumber() + ".json")) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             JsonObject jsonObj = new JsonObject();
@@ -85,6 +80,9 @@ public class JsonReaderWriter{
             }
             gson.toJson(jsonObj, writer);
             return true;
-        } 
+        }
+        catch(Exception e){
+            return false;   
+        }
     }
-}
+}      
