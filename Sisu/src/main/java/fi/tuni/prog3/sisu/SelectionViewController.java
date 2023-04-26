4721bad42a4aeff9a6a49f1a60ee0baa0138b924
Elsa Seppänen <elsa.seppanen@tuni.fi>
2023-04-26 19:07:32 +0300

    FXML file for selection view and methods added to both controller classes.

--- Sisu/src/main/java/fi/tuni/prog3/sisu/SelectionViewController.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/SelectionViewController.java
@@ -1,11 +1,23 @@
 
 package fi.tuni.prog3.sisu;
 
+import fi.tuni.prog3.sisu.files.JsonReaderWriter;
+import fi.tuni.prog3.sisu.kori.Data;
+import fi.tuni.prog3.sisu.modules.Course;
+import fi.tuni.prog3.sisu.modules.DegreeProgramme;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.TreeSet;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 
 /**
  * FXML Controller class
@@ -18,13 +30,95 @@ public class SelectionViewController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
-    private ListView<String> degreeListView;
+    private Tab studentInfoTab;
+    
+    @FXML
+    private ListView<String> chooseDegreeListView;
+    
+    @FXML
+    private Label degreeProgLabel;
+    
+    @FXML            
+    private Label firstNameLabel;
+    
+    @FXML
+    private Label lastNameLabel;
+    
+    @FXML
+    private Label studentNumberLabel;
+    
+    @FXML
+    private Label startYearLabel;
+    
+    @FXML
+    private Label gradYearLabel;
+    
+    @FXML
+    private Label creditsLabel;
+    
+    @FXML
+    private Button logOutButton;
+        
+    private final Data sisu = new Data();
+    private final HashMap<String, DegreeProgramme> degrees = sisu.getDegrees();
+    private Student user = new Student();
+    String currentDegree; 
             
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        showDegrees();
+        logOutButton.setOnAction(this::handleLogOutButtonAction);  
+    }
+    
+    private void handleLogOutButtonAction(ActionEvent e)
+    {
+        JsonReaderWriter writer = new JsonReaderWriter();
+        try {
+            writer.writeToFile(user);
+            Sisu.setRoot("startDialogue");
+        } catch (Exception ex) {
+        }
+        
+    }
+    
+    /**
+     * Shows the degrees in the list view and lets the user choose and change
+     * a degree programme.
+     */
+    private void showDegrees() {
+        TreeSet<String> degreeNames = new TreeSet<>();
+        for (DegreeProgramme prog : degrees.values())
+        {
+            degreeNames.add(prog.getName());
+        }
+        chooseDegreeListView.getItems().addAll(degreeNames);
+                chooseDegreeListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> ov, String t, String t1) {
+                currentDegree = chooseDegreeListView.getSelectionModel().getSelectedItem();
+                degreeProgLabel.setText(currentDegree);
+            }
+        }); 
+    }
+        
+    private void chooseDegree() {
+        user.setDegree(currentDegree);
+    }
+    
+    private void showCredits() {
+        TreeSet<String> completed = user.getCompletedCourses();
+        
+              
     }
 
-    
+    public void initData(Student loggedUser) {
+        user = loggedUser;
+        firstNameLabel.setText(user.getFirstName());
+        lastNameLabel.setText(user.getLastName());
+        studentNumberLabel.setText(user.getStudentNumber());
+        startYearLabel.setText(Integer.toString(user.getStartYear()));
+        gradYearLabel.setText(Integer.toString(user.getGradYear()));
+        degreeProgLabel.setText(user.getDegree());                
+    }
     
 }
--- Sisu/src/main/java/fi/tuni/prog3/sisu/StartDialogueController.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/StartDialogueController.java
@@ -4,19 +4,21 @@ import fi.tuni.prog3.sisu.files.JsonReaderWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-
+import javafx.stage.Stage;
 /**
  * FXML Controller class
+ *
  */
 public class StartDialogueController implements Initializable {
 
@@ -45,66 +47,86 @@ public class StartDialogueController implements Initializable {
     @FXML
     private Label logInErrorLabel;
     
+    private Student newStudent;
+    
     /**
      * Initializes the controller class.
-     * @param url
-     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         startHBox.setStyle("-fx-background-color: #6D94BD;");
         startSeparator.setStyle("-fx-background-color: #88B4D1;");
         
-        signUpButton.setOnAction(this::handleSignUpButtonAction);
-        
+        signUpButton.setOnAction(this::handleSignUpButtonAction);        
         logInButton.setOnAction(this::handleLogInButtonAction);                       
         }
     
     /**
-     * 
+     * Handles the event, when sign up button is pressed.
+     * Checks if info is correct and creates a new student.
      * @param event 
      */
-    private void handleSignUpButtonAction(ActionEvent event) {
+    private void handleSignUpButtonAction(ActionEvent event) {    
+        if ("".equals(firstNameField.getText().trim()) ||
+                "".equals(lastNameField.getText().trim()) ||
+                "".equals(startYearField.getText().trim()) ||
+                "".equals(gradYearField.getText().trim()) ||
+                "".equals(newStudentNumberField.getText().trim())) {
+            signUpErrorLabel.setText("Enter all information!");
+            return;            
+        }
+        
         String firstName = firstNameField.getText();
         String lastName = lastNameField.getText();
         int startYear = Integer.parseInt(startYearField.getText());
         int gradYear = Integer.parseInt(gradYearField.getText());
         String studentNumber = newStudentNumberField.getText();
-        if ("".equals(firstNameField.getText()) || lastNameField.getText() == null
-            || startYearField.getText() == null || gradYearField.getText()
-            == null || newStudentNumberField.getText() == null) {
-            signUpErrorLabel.setText("Enter all information!");
-        }
-        Student newStudent = new Student(firstName, lastName, studentNumber,
-                startYear, gradYear);
-        }
-    
-    /**
-     * 
-     * @param event 
-     */
-    private void handleLogInButtonAction(ActionEvent event) {
         
         try {
-            String studentNumber = studentNumberField.getText();
-            if ("".equals(studentNumber))
+            if (checkStudent(studentNumber))
             {
-                logInErrorLabel.setText("Enter a student number!");
+                signUpErrorLabel.setText("Student number already exists!");
+            }
+        }
+        
+        catch (Exception ex) {
+        }
+        
+        Student student = new Student(firstName, lastName, studentNumber,
+                startYear, gradYear);
+        
+        try {
+            newStudent = student;
+            moveStudentData();
+        }
+        catch (IOException ex) {
+        }
+    }
+    
+    /**
+     * Handles the event, when log in button is clicked.
+     * @param event 
+     */
+    private void handleLogInButtonAction(ActionEvent event){
+        
+        String studentNumber = studentNumberField.getText();
+        if ("".equals(studentNumber.trim()))
+        {
+            logInErrorLabel.setText("Enter a student number!");
+        }
+        
+        else try {
+            if(checkStudent(studentNumber))
+            {               
+                moveStudentData();
             }
             else
-            {
-                  Sisu.setRoot("selectionView");
-//                if(checkStudent(studentNumber))
-//                {
-//                    Sisu.setRoot("selectionView");
-//                }
-//                else
-//                {
-//                    logInErrorLabel.setText("Wrong student number!");
-//                }
+            {               
+                logInErrorLabel.setText("Wrong student number!");
             }
-        } catch (IOException ex) {
-            Logger.getLogger(StartDialogueController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (Exception ex) {
         }
     }
     
@@ -113,10 +135,33 @@ public class StartDialogueController implements Initializable {
      * @param studentNumber
      * @return 
      */
-    private boolean checkStudent(String studentNumber) throws Exception
-    {
+    private boolean checkStudent(String studentNumber) throws Exception {
         JsonReaderWriter checker = new JsonReaderWriter();
         Student test = checker.readFromFile(studentNumber);
+        if (test != null)
+        {
+            newStudent = test;
+        }
         return test != null;
     }
+    
+    /**
+     * 
+     * @throws IOException 
+     */
+    public void moveStudentData() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("selectionView.fxml"));
+
+        Parent root = loader.load();
+        Scene selection = new Scene(root);
+        
+        SelectionViewController controller = loader.getController();
+        controller.initData(newStudent);
+        
+        Stage primaryStage = (Stage) logInButton.getScene().getWindow();
+        primaryStage.setScene(selection);
+        primaryStage.show();
+    }
+         
 }   
